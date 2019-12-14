be28c1031d7b5c4a8fb3d3b65e193805bad3962c
David Millar-Durrant <dmillardurrant@gmail.com>
2019-12-14 22:40:59 +0100

    Improved build for org

--- build.sh
+++ build.sh
@@ -3,13 +3,9 @@
 set -eu
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-mkdir -p "$parent_path/public"
-cd "$parent_path/public"
-
 cp -r ../resources/* .
 
-mkdir -p p
-pandoc -s ../entries/Shaking-up-the-IDE.md -o p/Shaking-up-the-IDE.html --css ../pandoc.css
-
 pandoc -s ../entries/index.md -o index.html --css pandoc.css
+
+cd entries
+make
